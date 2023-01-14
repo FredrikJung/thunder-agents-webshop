765e01d1250a6e61a7f5bb9e5ef3d3a592a41a68
FredrikJung <fredrik.jung@hotmail.com>
2023-01-14 12:53:53 +0100

    Added useRef hook and added test code in shop page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { Navbar } from './Components/navbar';
-import { Shop } from './pages/shop/shop'
-import { Cart } from './pages/cart/cart'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./Components/navbar";
+import { Shop } from "./pages/shop/shop";
+import { Cart } from "./pages/cart/cart";
 
 function App() {
   return (
     <div className="App">
-
       <Router>
         <Navbar />
         <Routes>
--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
+import useFetch from "../../Components/useFetch";
 
 export const Shop = () => {
+  const { data: products } = useFetch("https://fakestoreapi.com/products");
+
   return (
-    <div>S</div>
-  )
-}
+    <div>
+      {products?.map((productsItem) => {
+        return <li key={productsItem.id}>{productsItem.title}</li>;
+      })}
+    </div>
+  );
+};
