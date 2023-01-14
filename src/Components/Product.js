edee64b438290b9497f7058959f74742effcf301
carllohr <calle.lohr@gmail.com>
2023-01-14 21:43:56 +0100

    div for button & input (product)

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,21 +1,22 @@
 import React from "react";
 
-
-const Product = ({product}) => {
-
-    const addToCart = () => {
-        console.log("adding " + product.title + " to cart");
-    }
-  return <div className="product">
-    
-    <img src={product.image} alt="product"></img>
-    <b></b>
-    <h3>{product.title}</h3>
-    <b></b>
-    <p>{product.price}$</p>
-    <input type="number"></input>
-    <button onClick={addToCart}>Add to cart</button>
-  </div>;
+const Product = ({ product }) => {
+  const addToCart = () => {
+    console.log("adding " + product.title + " to cart");
+  };
+  return (
+    <div className="product">
+      <img src={product.image} alt="product"></img>
+      <b></b>
+      <h3>{product.title}</h3>
+      <b></b>
+      <p>{product.price}$</p>
+      <div className="product-input">
+        <input type="number"></input>
+        <button onClick={addToCart}>Add to cart</button>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
