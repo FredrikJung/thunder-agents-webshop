dd95b4a9ce3089fb3d016b8fa4bfd87ba67a68e2
jesperforsen2022 <lester_orkester@hotmail.com>
2023-01-15 21:26:09 +0100

    navbar css added

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Link } from "react-router-dom"
+import "./navbar.css"
+import TAgents from './images/TAgents.webp'
 
 export const Navbar = () => {
   return (
-    <div>
-        <div>
+    <div className="navbar">
+        <div className="links">
             <Link to="/"> Shop </Link>
-            <Link to="/cart"> Cart </Link>
+           <div> <img className="thunderAgents" src={TAgents} alt="Thunder Agents" /></div>
+            <Link to="/cart"> Cart
+                </Link>
         </div>
     </div>
   )
