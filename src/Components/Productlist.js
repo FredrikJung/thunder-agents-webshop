3611f49843cf6047bf572f88c2b4d4c100d6d627
carllohr <calle.lohr@gmail.com>
2023-01-16 14:55:48 +0100

    filter function added

--- src/Components/Productlist.js
+++ src/Components/Productlist.js
@@ -1,6 +1,7 @@
 import React from "react";
 import UseFetch from "./UseFetch";
 import Product from "./Product";
+import {useState} from "react";
 
 const Productlist = () => {
   const {
@@ -9,11 +10,30 @@ const Productlist = () => {
     error,
   } = UseFetch("https://fakestoreapi.com/products");
 
+  const [category, setCategory] = useState("all");
+
   return (
     <div className="products">
       {error && <div>{error}</div>}
       {loading && <div>Loading...</div>}
-      {products?.map((product) => {
+      <select name="category" value={category} onChange={(e) => {
+        const selectedCategory = e.target.value;
+        setCategory(selectedCategory);
+      }}>
+        <option value="all">All</option>
+        <option value="men's clothing" >Men's clothing</option>
+        <option value="jewelery" >Jewelery</option>
+        <option value="electronics" >Electronics</option>
+        <option value="women's clothing" >Women's clothing</option>
+      </select>
+      {products?.filter((product) => {
+        if(product.category === category){
+          return product;
+        }
+        else if(category === "all"){
+          return products;
+        }
+      }).map((product) => {
         return <Product key={product.id} product={product} />;
       })}
     </div>
