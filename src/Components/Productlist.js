bf016f7bb46f5ce606b29b00f294dcc124357a35
FredrikJung <fredrik.jung@hotmail.com>
2023-01-16 17:20:15 +0100

    Added imported selectbox from MUI

--- src/Components/Productlist.js
+++ src/Components/Productlist.js
@@ -1,7 +1,12 @@
 import React from "react";
 import UseFetch from "./UseFetch";
 import Product from "./Product";
-import {useState} from "react";
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 const Productlist = ({ addToCart }) => {
   const {
@@ -13,29 +18,52 @@ const Productlist = ({ addToCart }) => {
   const [category, setCategory] = useState("all");
 
   return (
-    <div className="products">
-      {error && <div>{error}</div>}
-      {loading && <div>Loading...</div>}
-      <select name="category" value={category} onChange={(e) => {
-        const selectedCategory = e.target.value;
-        setCategory(selectedCategory);
-      }}>
-        <option value="all">All</option>
-        <option value="men's clothing" >Men's clothing</option>
-        <option value="jewelery" >Jewelery</option>
-        <option value="electronics" >Electronics</option>
-        <option value="women's clothing" >Women's clothing</option>
-      </select>
-      {products?.filter((product) => {
-        if(product.category === category){
-          return product;
-        }
-        else if(category === "all"){
-          return products;
-        }
-      }).map((product) => {
-        return <Product key={product.id} product={product} addToCart={addToCart}/>;
-      })}
+    <div>
+      <div className="filterBox">
+        <Box sx={{ minWidth: 120 }}>
+          <FormControl>
+            <InputLabel id="demo-simple-select-label">Categories</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={category}
+              label="Categories"
+              onChange={(e) => {
+                const selectedCategory = e.target.value;
+                setCategory(selectedCategory);
+              }}
+            >
+              <MenuItem value="all">All products</MenuItem>
+              <MenuItem value="men's clothing">Men's clothing</MenuItem>
+              <MenuItem value="women's clothing">Women's clothing</MenuItem>
+              <MenuItem value="jewelery">Jewelery</MenuItem>
+              <MenuItem value="electronics">Electronics</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+      </div>
+      <div className="products">
+        {error && <div>{error}</div>}
+        {loading && <div>Loading...</div>}
+        {products
+          ?.filter((product) => {
+            if (product.category === category) {
+              return product;
+            } else if (category === "all") {
+              return products;
+            }
+            return false;
+          })
+          .map((product) => {
+            return (
+              <Product
+                key={product.id}
+                product={product}
+                addToCart={addToCart}
+              />
+            );
+          })}
+      </div>
     </div>
   );
 };
