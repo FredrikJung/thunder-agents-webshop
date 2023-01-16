8ada0a5c0246f601686c89634ab02cca9390eb58
FredrikJung <fredrik.jung@hotmail.com>
2023-01-16 15:38:52 +0100

    sdsdsd

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,11 +1,9 @@
-import { useEffect, useState } from "react";
 import Productlist from "../../Components/Productlist";
 
 export const Shop = ({ addToCart }) => {
-
   return (
     <div className="container">
-      <Productlist addToCart = { addToCart }/>
+      <Productlist addToCart={addToCart} />
     </div>
   );
 };
