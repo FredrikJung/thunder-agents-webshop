6f1e49be992e471f69bf946d930b3b7f49c153b2
FredrikJung <fredrik.jung@hotmail.com>
2023-01-18 11:33:13 +0100

    Changed text for h1 in orderPlaced page

--- src/pages/orderPlaced/orderPlaced.jsx
+++ src/pages/orderPlaced/orderPlaced.jsx
@@ -1,7 +1,7 @@
 export const OrderPlaced = () => {
   return (
     <div className="order-placed">
-      <h1>Thank you for purchase!</h1>
+      <h1>Thank you for your purchase!</h1>
       <h2>
         Your order has been confirmed and you will soon get an email with all
         the orderdetails
